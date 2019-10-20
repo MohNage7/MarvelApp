0d06d06c878fec219575d6f2991368811047a97f
Mohamed Nageh <nageh@zadfresh.com>
2019-10-20 11:11:34 +0200

    - Removed unused resources.

--- app/src/main/java/com/mohnage7/marvelapp/utils/Constants.java
+++ app/src/main/java/com/mohnage7/marvelapp/utils/Constants.java
@@ -2,12 +2,11 @@ package com.mohnage7.marvelapp.utils;
 
 public class Constants {
     public static final String BASE_URL = "https://gateway.marvel.com/v1/public/";
-    public static final String PRIVATE_KEY = "0f1d0fdf46a0bf32f962b0b9997233c0395cdf8e";
-    public static final String PUBLIC_KEY = "6a7ed890b4b941a925202a5630d5b162";
+    public static final String PRIVATE_KEY = "";
+    public static final String PUBLIC_KEY = "";
     public static final int CONNECTION_TIMEOUT = 10; // 10 seconds
     public static final int READ_TIMEOUT = 60; //  seconds
     public static final int WRITE_TIMEOUT = 60; // seconds
-    public static final int CACHE_TIMEOUT = 2; //  minutes
     //Limit the result set to the specified number of resources.
     public static final int LIMIT= 20;
     //Skipped number of resources in the result set of characters API.
